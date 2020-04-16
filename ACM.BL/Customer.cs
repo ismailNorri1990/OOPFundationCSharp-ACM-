0e992bb8071c845f55f994371cf7559ec863e8e0
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-16 14:27:11 +0000

    Creation des class: Product ,OrderItem, Order

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM.BL
 {
     public class Customer
     {
+
+        public Customer()
+        {
+                    
+        }
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
+
         public int CustomerId { get; private set; }
         public string EmailAdress { get; set; }
 
@@ -39,6 +51,57 @@ namespace ACM.BL
             {
                 _lastName = value;
             }
+
+        }
+
+        /// <summary>
+        /// Retrieves All Customers
+        /// </summary>
+        /// <return></return>
+
+        public List <Customer> Retrieves()
+        {
+            
+            return new List<Customer>();
+
+        }
+
+        /// <summary>
+        /// Retrieves the customer data
+        /// </summary>
+
+        public Customer Retrieve(int customerId)
+        {
+            //Code that retrieve one customer
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Saves the customer data.
+        /// </summary>
+        /// <return></return>
+
+        public bool Save()
+        {
+            //Code that saves the defined customer
+
+            return true;
+
+        }
+
+        /// <summary>
+        /// Validate the customer data
+        /// </summary>
+        /// <return></return>
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAdress)) isValid = false;
+
+            return isValid;
 
         }
     }
